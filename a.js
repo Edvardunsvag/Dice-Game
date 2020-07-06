655dfff4a157662d9c7a73970443b5a74abdc89c
edvardunsvag <edvardhu@student.matnat.uio.no>
2020-07-07 00:24:28 +0200

    k

--- a.js
+++ a.js
@@ -1,27 +1,24 @@
 var express = require('express');
 var app = express();
-var bodyParser = require("body-parser");
-
+var bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname+'/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.get('public/style.css', function(req, res) {
-  res.sendFile(__dirname + 'public/style.css");
+app.get('style.css', function (req, res) {
+    res.sendFile(__dirname + 'style.css');
 });
 
-app.post("/", function(req, res){
+app.post('/', function (req, res) {
     console.log(req.body.submit);
 });
 
-
-
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Listening on port 3000");
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Listening on port 3000');
 });
